2111c7076a6f75c6c6707d8a9ff1851d939d9a9d
Matt Groff <mattlgroff@gmail.com>
2017-11-09 03:40:49 -0800

    Using Math.round and a parseFloat instead of parseInts, which was rounding DOWN in the wrong places instead of rounding UP

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.engine("handlebars", exphbs(
     defaultLayout: "main",
     helpers:{
       caloriesFromFat: totalFat => parseInt(totalFat) * 9,
-      percentDailyTotalFat: totalFat => parseInt((parseInt(totalFat) / 64) * 100),
-      percentDailySatFat: satFat => parseInt((parseInt(satFat) / 20) * 100),
-      percentDailyChol: chol => parseInt((parseInt(chol) / 300) * 100),
-      percentDailySodium: sodium => parseInt((parseInt(sodium) / 2400) * 100),
-      percentDailyTotalCarb: totalCarb => parseInt((parseInt(totalCarb) / 300) * 100),
-      percentDailyDietaryFiber: dietaryFiber => parseInt((parseInt(dietaryFiber) / 25) * 100)
+      percentDailyTotalFat: totalFat => Math.round(parseFloat(parseInt(totalFat) / 64) * 100),
+      percentDailySatFat: satFat => Math.round(parseFloat(parseInt(satFat) / 20) * 100),
+      percentDailyChol: chol => Math.round(parseFloat(parseInt(chol) / 300) * 100),
+      percentDailySodium: sodium => Math.round(parseFloat(parseInt(sodium) / 2400) * 100),
+      percentDailyTotalCarb: totalCarb => Math.round(parseFloat(parseInt(totalCarb) / 300) * 100),
+      percentDailyDietaryFiber: dietaryFiber => Math.round(parseFloat(parseInt(dietaryFiber) / 25) * 100)
     }
   }
 ));
