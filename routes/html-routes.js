5278ff3606068ebd1538e48bdc9d8af666d836f8
Matt Groff <mattlgroff@gmail.com>
2017-11-09 01:34:59 -0800

    Added public folder, with a placeholder index.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 module.exports = app => {
   //TODO HTML ROUTES
-  app.use(express.static("../public"));
+  app.use(express.static(path.join(__dirname, './../public')));
 }
