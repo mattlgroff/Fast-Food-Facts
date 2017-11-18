d9f07dd28a2dcb66a2148d17bd24667e83599b0c
Matt Groff <mattlgroff@gmail.com>
2017-11-18 10:49:59 -0800

    Added carlos auth branch

---
+++ routes/authRoutes.js
@@ -0,0 +1,29 @@
+var authController = require('../controllers/authController.js');
+
+module.exports = function(app, passport) {
+
+    app.get('/signin', authController.signin);
+
+    app.post('/signin', passport.authenticate('local-signup', {
+            successRedirect: '/dashboard',
+
+            failureRedirect: '/signin'
+        }
+
+    ));
+
+    app.get('/dashboard', isLoggedIn, authController.dashboard);
+    
+    app.get('/logout', authController.logout);
+
+};
+
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated())
+
+        return next();
+
+    res.redirect('/signin');
+
+}
