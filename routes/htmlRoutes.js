d4759ec745a30376acfb5c64097a797d0912a573
Matt Groff <mattlgroff@gmail.com>
2017-11-18 11:45:19 -0800

    Added auth check to main.handlebars and pass into every object

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,17 +13,17 @@ module.exports = app => {
 
 
   app.get("/search", (req, res) => {
-    res.render("search", null);
+    res.render("search", req.user);
   });
 
 
   //Create Food Form
   app.get("/create", (req, res) => {
     //TODO: Null will be replaced with the User object containing the logged in user
-    res.render("createFood",null);
+    res.render("createFood", req.user);
   });
 
   app.post("/create", (req, res) => {
-    htmlController.createFood(req.body, res);
+    htmlController.createFood(req, res);
   });
 }
